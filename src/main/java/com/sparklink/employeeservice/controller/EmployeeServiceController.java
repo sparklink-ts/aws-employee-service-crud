d0c190113b38a632f5125bc245c4541ed20ff26b
sparklink-ts <sparklink.ts@gmail.com>
2026-02-18 21:02:04 +0530

    Updated with new version of Employee Service

--- src/main/java/com/sparklink/employeeservice/controller/EmployeeServiceController.java
+++ src/main/java/com/sparklink/employeeservice/controller/EmployeeServiceController.java
@@ -35,7 +35,7 @@ public class EmployeeServiceController {
 	//@GetMapping("/getEmployeeServicesDeployments")
 	@GetMapping("${rest.get.mapping.getEmployeeServicesDeployments}")
 	public String getEmployeeServicesDeployments() {
-		return "AWS - Employee Services CICD Working Fine :  ...";
+		return "Employee Services Microservice with Spring Boot CICD Devlopment, Build and Deployment Working Fine With AWS Services : RDS - MySQL Database, S3, EC2, Elastic Beanstalk, Code Pipeline, Code Build and SNS ...";
 	}
 
 	@Transactional
